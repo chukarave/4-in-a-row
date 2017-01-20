2915cfc220f0d7c5200df1d0be038e2e3a9e1fff
chukarave <noa.rave@berlinonline.de>
2017-01-20 14:44:50 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string alphabet = "ABCDEFGHIJ";
+			Console.WriteLine("Please select coordinates: ");
+			string answer = Console.ReadLine();
+			Console.WriteLine (alphabet.IndexOf(answer[0], 0));
+
+        }
+        public static void updateboard()
+        { 
+			//string[,] board = new string[10, 10]; 
+			string alphabet = "ABCDEFGHIJ";
+            int c, i, j;
+            bool firstiteration = true;
+            
+
+
+            for (c = 1; c < 11; c++)
+            {
+                if (firstiteration)
+                {
+                    Console.Write(" ");
+                    firstiteration = false;
+                }
+                Console.Write(" " + c + " ");
+            }
+            Console.WriteLine();
+
+         
+            for (i = 0; i < 10; i++ )
+            {
+                Console.Write(alphabet[i]);
+                for (j = 0; j < 10; j++ )
+                {
+					//board[i, j] = ;
+               }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ drawboard().cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            drawboard();
+        }
+        public static void drawboard()
+        { 
+            
+            string alphabet = "ABCDEFGHIJ";
+            int c, i, j;
+            bool firstiteration = true;
+            
+            for (c = 1; c < 11; c++)
+            {
+                if (firstiteration)
+                {
+                    Console.Write(" ");
+                    firstiteration = false;
+                }
+                Console.Write(" " + c + " ");
+            }
+            Console.WriteLine();
+
+         
+            for (i = 0; i < 10; i++ )
+            {
+                Console.Write(alphabet[i]);
+                for (j = 0; j < 10; j++ )
+                {
+                    
+                    Console.Write(" _ ");
+               }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
