dd0c871c6abba6f467496aeb0bbad5b57df51d87
noa <noa@noa.home>
2017-02-12 12:47:41 +0100

    add check methods for horizontal or vertical winner

--- Board.cs
+++ Board.cs
@@ -17,8 +17,9 @@ using System.Text;
 	    board = new string[nr,nc];
 	  
 	}
-
-        public void drawboard()
+	
+	// Draw initial board.
+        public void drawBoard()
         { 
             
             int  i, j;
@@ -30,7 +31,7 @@ using System.Text;
                    if (board[i, j] == null)
 		   {
 		   
-                    Console.Write("n");
+                    Console.Write("_");
 		   } 
                     Console.Write(board[i, j]+" ");
                }
@@ -45,24 +46,57 @@ using System.Text;
 
         }
 
-        public bool updateboard(string color, int numcol)
+        public int updateBoard(string color, int numcol)
         { 
             int row;
-//	    bool updated = false;
 
        	//iterate over rows and check if spot is occupied 
             for (row = nr-1; row >= 0; row--)
             { 
-			if (board[row, numcol] == null) 
-			{
-		  	    board[row, numcol] = color;
-			    return true;
-			}
+		if (board[row, numcol] == null) {
+	  	    board[row, numcol] = color;
+		    return row;
+		}
             }
 
-	    return false;
+	    return -1;
 
         }
 
+	// Check if the same color repeats 4 consective times in a column.
+	public bool checkVertical(int col, string color)
+	{
+		int row;
+		int counter = 0;
+		for (row = 0; row < nr; row++){
+			if (color.Equals(board[row, col])) {
+			    counter++;		
+			    if (counter == 4)
+			   	 { return true; }
+			} else {
+			    counter = 0;
+			}
+		}
+
+		return false;
+	}
+	
+	// Check if the same color repeats 4 consective times in a row.
+	public bool checkHorizontal(int row, string color)
+	{
+		int col;
+		int counter = 0;
+		for (col = 0; col < nc; col++){
+			if (color.Equals(board[row, col])) {
+			    counter++;		
+			    if (counter == 4)
+			   	 { return true; }
+			} else {
+			    counter = 0;
+			}
+		}
+
+		return false;
+	}
 
     }
